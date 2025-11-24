89fafd04872f9f60d3444e97802929b8b2dbf006
timChen <tingyang.chen@colatour.com.tw>
2025-11-24 15:38:13 +0800

    fix: update addBrickToMap to prevent out-of-bounds errors and return a copy of the map

--- app/lib/map.ts
+++ app/lib/map.ts
@@ -17,12 +17,17 @@ export const addBrickToMap = (tetrisMap: Map, brick: Brick): Map => {
   const brickShape = brick.shapes[0]; //todo: handle multiple shapes
   const startX = brick.position.x;
   const startY = brick.position.y;
+  const copyMap: Map = tetrisMap.map(row => [...row]);
   for (let i = 0; i < brickShape.length; i++) {
     for (let j = 0; j < brickShape[i].length; j++) {
-      if (brickShape[i][j] === 1) {
-        tetrisMap[startY + i][startX + j] = BRICK_MAP_VALUE;
+      if (
+        brickShape[i][j] === 1 &&
+        startY + i < MAP_HEIGHT &&
+        startX + j < MAP_WIDTH
+      ) {
+        copyMap[startY + i][startX + j] = BRICK_MAP_VALUE;
       }
     }
   }
-  return tetrisMap;
+  return copyMap;
 };
