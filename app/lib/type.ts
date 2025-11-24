2239251cf6b578b9720a03a77d22f849ae1ab0d5
timChen <tingyang.chen@colatour.com.tw>
2025-11-24 15:27:27 +0800

    add type setting

---
+++ app/lib/type.ts
@@ -0,0 +1,17 @@
+const EMPTY_MAP_VALUE = 0;
+const BRICK_MAP_VALUE = 1;
+const MAP_WIDTH = 10;
+const MAP_HEIGHT = 20;
+
+type Map = (0 | 1)[][];
+
+type Brick = {
+  position: {
+    x: number;
+    y: number;
+  };
+  shapes: (0 | 1)[][][];
+}
+
+export type { Map, Brick };
+export { EMPTY_MAP_VALUE, BRICK_MAP_VALUE, MAP_WIDTH, MAP_HEIGHT };
